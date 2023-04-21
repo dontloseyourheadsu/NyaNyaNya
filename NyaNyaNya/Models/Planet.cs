b875c06098dc1f30f13fa49929c8ed4053ac1d6d
Jesus Alvarez <dontloseyourheadsu@gmail.com>
2023-04-21 14:22:03 -0600

    Added play/extras and planet images

---
+++ NyaNyaNya/Models/Planet.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NyaNyaNya.Models
+{
+    public class Planet
+    {
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+
+        public bool IsActive { get; set; }
+        public int Index { get; set; }
+
+        public Planet(bool isActive, int index)
+        {
+            IsActive = isActive;
+            Index = index;
+        }
+
+        public void PlayPlanet()
+        {
+            if(IsActive)
+            {
+                NavigationManager.NavigateTo("/game/" + Index);
+            }
+        }
+    }
+}
